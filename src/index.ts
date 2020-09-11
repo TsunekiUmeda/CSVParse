b1ef13003515308c8e64abedbf0d3fd5eafb5ae5
Umeda <umedat@f8ffc22a8bb2.ant.amazon.com>
2020-09-11 16:50:47 +0900

    Add dataStringToDate function and upgrade argumaent data type as MatchData

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import { CsvFileReader } from './CsvFileReader'
+import { MatchResult } from './MatchResult'
 const reader = new CsvFileReader('football.csv')
-reader.read()
 
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
+reader.read()
 
 let manUnitedWins = 0
 
---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export const dataStringToDate = (dataString: string): Date => {
+  //28/20/2018
+  const dateParts = dataString.split('/').map((value: string): number => {
+    return parseInt(value)
+  })
+  return new Date(dateParts[2], dateParts[1] - 1, dateParts[0])
+}
